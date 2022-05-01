60df6275f6a704b4a1b1034720d957e476ee1a73
berkan-alci <78726455+berkan-alci@users.noreply.github.com>
2022-05-01 22:37:37 +0200

    nginx ingress fix + addition error middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,24 +1,22 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter, signInRouter, signUpRouter, signOutRouter } from './routes';
+import { errorHandler } from './middlewares/error-handler';
+
+
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {  res.send('Hi there');});
 
-//routes
-app.get('/', async (req, res) => {
-    console.log('reached')
-    res.send('Hi there');
-});
-
-
-app.get('/api/users/currentuser', async (req, res) => {
-    console.log('reached')
-    res.send('Hi there');
-});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
+app.use(errorHandler);
 
 
-
-app.listen(4000, () => {
-    console.log("TICKET-AUTH-MS : 4000 t" );
+app.listen(3000, () => {
+    console.log("TICKET-AUTH-MS - PORT:3000" );
 });

3e3c6b8bd25283da2d0a6590d93db8c133641434
berkan-alci <78726455+berkan-alci@users.noreply.github.com>
2022-05-02 16:58:05 +0200

    next.js signup

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -1,7 +1,7 @@
 export default () => {
     return (
         <div>
-            Banana page
+            Sigin
         </div>
     );
 }
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import axios from 'axios';
+
+export default () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const res = await axios.post('http://ticketing.com/api/users/signup', { email, password });
+        console.log(res.data);
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>Sign Up</h1>
+            <div className="form-group">
+                <label>Email:</label>
+                <input value={email} onChange={e => setEmail(e.target.value)} className="form-control" type="email" />
+            </div>
+            <div className="form-group">
+                <label>Password:</label>
+                <input value={password} onChange={e => setPassword(e.target.value)} className="form-control" type="password" />
+            </div>
+            <button className="btn btn-primary">Sign Up</button>
+        </form>
+    );
+}

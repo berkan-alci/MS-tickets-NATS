60e62f493f987f8585067d31a48927fa9ed76fd6
berkan-alci <78726455+berkan-alci@users.noreply.github.com>
2022-05-06 22:46:01 +0200

    test for new order + get request handlers done

--- orders/src/routes/all-orders.ts
+++ orders/src/routes/all-orders.ts
@@ -1,9 +1,15 @@
+import { requireAuth } from '@tt-ms-common/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models';
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
+router.get('/api/orders', requireAuth,  async (req: Request, res: Response) => {
+    const orders = await Order.find({
+        userId: req.currentUser!.id
+    }).populate('ticket');
 
+    res.send(orders);
 });
 
 export default router;
--- orders/src/routes/show-order.ts
+++ orders/src/routes/show-order.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
-
+import { AuthorizationError, NotFoundError, requireAuth } from '@tt-ms-common/common';
+import { Order } from '../models';
 const router = express.Router();
 
-router.get('/api/orders/:id', async (req: Request, res: Response) => {
-
+router.get('/api/orders/:orderId',requireAuth, async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+    if (!order) {
+        throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+        throw new AuthorizationError();
+    }
+    
+    res.send(order)
 });
 
 export default router;
